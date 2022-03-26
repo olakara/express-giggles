459a39f5ec68f0276cffc9dfbaa615c2e87b2e36
Abdel Raoof <olakara@gmail.com>
2022-03-26 12:50:03 +0400

    adding a new router

--- app.js
+++ app.js
@@ -3,6 +3,21 @@ const express = require('express');
 const app = express();
 
 const port = process.env.PORT || 5000;
+
+const buildersRouter = express.Router();
+
+buildersRouter.route('/builders').get((req, res) => {
+  const values = [{
+    name: 'Honda',
+  }, {
+    name: 'Volvo',
+  }];
+
+  return res.json(values);
+});
+
+app.use('/api', buildersRouter);
+
 app.listen(port, () => {
   console.log('Running on PORT: ', port);
 });
